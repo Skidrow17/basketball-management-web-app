d466851b18f7aaa81a99bb5c3a5dca33a64c1ffc
Silvan Sholla <silvansholla@gmail.com>
2020-01-02 13:27:08 +0200

    state field added on the query

--- apk/json/getWeeklyMatches.php
+++ apk/json/getWeeklyMatches.php
@@ -13,15 +13,13 @@ $sql = "SELECT distinct
 		ON r.team_id_1 = home.id
 		JOIN team AS away 
 		ON r.team_id_2 = away.id , court c , team t where yearweek(r.date_time,1) = yearweek(curdate(),1) 
-		AND r.team_id_1=t.id AND t.category=:id And C.id=r.court_id order by date_time desc ";
+		AND r.team_id_1=t.id And C.id=r.court_id order by date_time desc ";
 		
 $run = $dbh->prepare($sql);
-$run->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
 $run->execute();
 $fetch = array();
-
+$fetch['ERROR']['error_code'] = "200";
 while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
 	$fetch['Match_Details'][] = $row;
 }
-
 echo json_encode($fetch);
