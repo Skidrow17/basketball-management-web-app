1c13630383e03cf493c0f91532c338f54426ca54
Silvan Sholla <silvansholla@gmail.com>
2020-04-12 13:31:57 +0300

    order small fix

--- apk/json/getReceivedMessages.php
+++ apk/json/getReceivedMessages.php
@@ -7,7 +7,7 @@ $fetch = array();
 if(isset($_GET['safe_key']) && isset($_GET['id'])){
 	$id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);		
 	if (security_check($_GET['safe_key'], $_GET['id']) == true) {
-		$sql = "SELECT M.id,U.name,U.surname,M.text_message,M.sender_id,U.profile_pic,DATE_FORMAT(M.date_time, '%d/%m/%Y %H:%i') as date_time,M.message_read FROM user U, message M where U.id=M.sender_id AND receiver_delete=0 AND receiver_id=:id ORDER BY date_time desc ";
+		$sql = "SELECT M.id,U.name,U.surname,M.text_message,M.sender_id,U.profile_pic,DATE_FORMAT(M.date_time, '%d/%m/%Y %H:%i') as date_time,M.message_read FROM user U, message M where U.id=M.sender_id AND receiver_delete=0 AND receiver_id=:id ORDER BY M.date_time desc ";
 		$run = $dbh->prepare($sql);
 		$run->bindParam(':id', $id, PDO::PARAM_INT);
 		$run->execute();
--- apk/json/getSentMessages.php
+++ apk/json/getSentMessages.php
@@ -7,7 +7,7 @@ $fetch = array();
 if(isset($_GET['safe_key']) && isset($_GET['id'])){
 	$id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);		
 	if (security_check($_GET['safe_key'], $_GET['id']) == true) {
-		$sql = "SELECT M.id,U.name,U.surname,M.text_message,U.profile_pic,DATE_FORMAT(M.date_time, '%d/%m/%Y %H:%i') as date_time,M.message_read FROM user U, message M where U.id=M.receiver_id AND sender_delete=0 AND sender_id=:id order by date_time desc";
+		$sql = "SELECT M.id,U.name,U.surname,M.text_message,U.profile_pic,DATE_FORMAT(M.date_time, '%d/%m/%Y %H:%i') as date_time,M.message_read FROM user U, message M where U.id=M.receiver_id AND sender_delete=0 AND sender_id=:id order by M.date_time desc";
 		$run = $dbh->prepare($sql);
 		$run->bindParam(':id', $id, PDO::PARAM_INT);
 		$run->execute();
