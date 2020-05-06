e3a5ee806bda87ee5dd1549bc8a6c46742ed5bff
Silvan Sholla <silvansholla@gmail.com>
2020-05-06 08:52:00 +0300

    autologot condition fix

--- assets/js/autologout.js
+++ assets/js/autologout.js
@@ -1,5 +1,5 @@
 function autologout(response) {	
-	if(response == 401) {
+	if(response == 401 || response == 'HTTP/1.0 401 Unauthorized') {
 		window.location.replace("./php/auto_logout.php");
 		wait(2000);
 	}
