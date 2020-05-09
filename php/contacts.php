66ff5e213ff1e3827d71e50e709bafe625037091
Silvan Sholla <silvansholla@gmail.com>
2020-05-09 12:32:59 +0300

    fix

--- php/contacts.php
+++ php/contacts.php
@@ -6,8 +6,10 @@
 require_once 'connect_db.php';
 if (isset($_SESSION['safe_key']) && isset($_SESSION['user_id'])) {
     if (security_check($_SESSION['safe_key'], $_SESSION['user_id']) == true) {
-        $sql2 = "SELECT id,name,surname FROM `user` order by id";
+        $user_id = $_SESSION['user_id'];
+        $sql2 = "SELECT id,name,surname FROM `user` WHERE active = 0 AND Id != :user_id order by id";
         $run = $dbh->prepare($sql2);
+        $run->bindParam(':user_id', $user_id, PDO::PARAM_INT);
         $run->execute();
         while ($row = $run->fetch()) {
             echo "<option class='dropdown-item' role='presentation' value=" . $row['id'] . " > " . $row['name'] . " " . $row['surname'] . "</option>";
