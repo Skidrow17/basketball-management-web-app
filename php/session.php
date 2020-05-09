75559b597b1ff3bb3af651c8e5a56e5b85a39045
Silvan Sholla <silvansholla@gmail.com>
2020-05-09 12:52:11 +0300

    deprecated method delete

--- php/session.php
+++ php/session.php
@@ -5,9 +5,6 @@
 
 session_start();
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 if (!isset($_SESSION['username'])) {
     if (isset($_COOKIE['uname']) == true && isset($_COOKIE['pwd']) == true) {
         header('location: ./php/login.php');
