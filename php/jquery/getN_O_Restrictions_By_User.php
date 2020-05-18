ee908ad24594757ca57f0e83365943e91e15c0fc
Silvan Sholla <silvansholla@gmail.com>
2020-05-18 21:07:29 +0300

    small fix on pagginatio

--- php/jquery/getN_O_Restrictions_By_User.php
+++ php/jquery/getN_O_Restrictions_By_User.php
@@ -8,12 +8,12 @@ require '../useful_functions.php';
 session_start();
 if (isset($_SESSION['safe_key']) && isset($_SESSION['user_id'])) {
     if (security_check($_SESSION['safe_key'], $_SESSION['user_id']) == true) {
-        $sql = "Select count(*) as n_o_r from restriction R where user_id=:uid";
+        $sql = "Select count(*) as n_o_r from restriction R where user_id=:uid AND R.deletable = 0 order by R.date desc";
         $run = $dbh->prepare($sql);
         $run->bindParam(':uid', $_SESSION['user_id'], PDO::PARAM_INT);
         $run->execute();
         while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
-            echo $row['n_o_r'] / 7;
+            echo $row['n_o_r'] / 4;
         }
     } else {
         session_destroy();
