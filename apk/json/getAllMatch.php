9c21d59befdf7b4495fd4df2a8dab00db98b692f
Silvan Sholla <silvansholla@gmail.com>
2020-05-22 20:37:30 +0300

    fix

--- apk/json/getAllMatch.php
+++ apk/json/getAllMatch.php
@@ -7,29 +7,31 @@ require_once '../../php/connect_db.php';
 
 $json_array = array();
 
-$sql = "SELECT distinct
-		home.name AS team_id_1, 
-		away.name AS team_id_2,r.id,r.team_score_1,r.team_score_2,DATE_FORMAT(r.date_time, '%d/%m/%Y %H:%i') as date_time,c.latitude,c.longitude,r.state
-		FROM 
-		game AS r
-		JOIN team AS home 
-		ON r.team_id_1 = home.id
-		JOIN team AS away 
-		ON r.team_id_2 = away.id , court c , team t where yearweek(r.date_time,1) = yearweek(curdate(),1) 
-		AND r.team_id_1=t.id AND t.category=:id And C.id=r.court_id order by date_time desc ";
-		
-$id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);		
-$run = $dbh->prepare($sql);
-$run->bindParam(':id', $id, PDO::PARAM_INT);
-$run->execute();
-$fetch = array();
-if ($run->rowCount() > 0) {
-	while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
-		$fetch['Match_Details'][] = $row;
-		$fetch['ERROR']['error_code'] = "200";
+if(isset($_GET["id"])){
+	$sql = "SELECT distinct
+			home.name AS team_id_1, 
+			away.name AS team_id_2,r.id,r.team_score_1,r.team_score_2,DATE_FORMAT(r.date_time, '%d/%m/%Y %H:%i') as date_time,c.latitude,c.longitude,r.state
+			FROM 
+			game AS r
+			JOIN team AS home 
+			ON r.team_id_1 = home.id
+			JOIN team AS away 
+			ON r.team_id_2 = away.id , court c , team t where yearweek(r.date_time,1) = yearweek(curdate(),1) 
+			AND r.team_id_1=t.id AND t.category=:id And C.id=r.court_id order by date_time desc ";
+			
+	$id = filter_var($_GET["id"], FILTER_SANITIZE_NUMBER_INT);		
+	$run = $dbh->prepare($sql);
+	$run->bindParam(':id', $id, PDO::PARAM_INT);
+	$run->execute();
+	$fetch = array();
+	if ($run->rowCount() > 0) {
+		while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
+			$fetch['Match_Details'][] = $row;
+			$fetch['ERROR']['error_code'] = "200";
+		}
+	} else {
+		$fetch['ERROR']['error_code'] = "204";
 	}
-} else {
-	$fetch['ERROR']['error_code'] = "204";
-}
 
-echo json_encode($fetch);
+	echo json_encode($fetch);
+}
--- apk/json/getRankingPerCategory.php
+++ apk/json/getRankingPerCategory.php
@@ -7,20 +7,22 @@ require_once '../../php/connect_db.php';
 require '../../php/useful_functions.php';
 $fetch = array();
 
-$cid = filter_var($_GET["cid"], FILTER_SANITIZE_NUMBER_INT);		
-$gid = filter_var($_GET["gid"], FILTER_SANITIZE_NUMBER_INT);		
+if(isset($_GET["cid"]) && isset($_GET["gid"])){
+	$cid = filter_var($_GET["cid"], FILTER_SANITIZE_NUMBER_INT);		
+	$gid = filter_var($_GET["gid"], FILTER_SANITIZE_NUMBER_INT);		
 
-$sql = "Select id,name,wins,loses,total_games,points from team where category = :cid AND team_group = :gid AND active = 0 ORDER BY points DESC";
-$run = $dbh->prepare($sql);
-$run->bindParam(':cid', $cid, PDO::PARAM_INT);
-$run->bindParam(':gid',	$gid, PDO::PARAM_INT);
-$run->execute();
-if ($run->rowCount() > 0) {
-	while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
-		$fetch['Team_Details'][] = $row;
-		$fetch['ERROR']['error_code'] = "200";
+	$sql = "Select id,name,wins,loses,total_games,points from team where category = :cid AND team_group = :gid AND active = 0 ORDER BY points DESC";
+	$run = $dbh->prepare($sql);
+	$run->bindParam(':cid', $cid, PDO::PARAM_INT);
+	$run->bindParam(':gid',	$gid, PDO::PARAM_INT);
+	$run->execute();
+	if ($run->rowCount() > 0) {
+		while ($row = $run->fetch(PDO::FETCH_ASSOC)) {
+			$fetch['Team_Details'][] = $row;
+			$fetch['ERROR']['error_code'] = "200";
+		}
+	} else {
+		$fetch['ERROR']['error_code'] = "204";
 	}
-} else {
-	$fetch['ERROR']['error_code'] = "204";
+	echo json_encode($fetch);
 }
-echo json_encode($fetch);
