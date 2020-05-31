b23546bc26c0e645d843b364a57556731b392006
Silvan Sholla <silvansholla@gmail.com>
2020-05-31 11:54:58 +0300

    query beuitfy

--- php/csv_export.php
+++ php/csv_export.php
@@ -12,7 +12,9 @@ require_once 'language.php';
 if(isset($_SESSION['safe_key']) && isset($_SESSION['user_id']) && isset($_SESSION['profession'])){
     if (security_check($_SESSION['safe_key'], $_SESSION['user_id']) == true && $_SESSION['profession'] == 'Admin') {
         if ($_GET['id'] == 1) {
-            $sql = "Select U.name,U.surname,R.time_from,R.time_to,R.date,R.register_timestamp,R.deletable,R.comment from restriction R, user U WHERE u.Id = R.user_id AND register_timestamp > :date_from";
+            $sql = "SELECT U.name,U.surname,R.time_from,R.time_to,R.date,R.register_timestamp,R.deletable,R.comment 
+                    FROM restriction R, user U 
+                    WHERE u.Id = R.user_id AND register_timestamp > :date_from";
             $filename = 'restriction-' . date('d.m.Y') . '.csv';
         } else if ($_GET['id'] == 3) {
             $sql = "SELECT U.name,U.surname,LH.login_date_time,LH.logout_date_time,LH.safe_key,LH.device_name,LH.ip 
@@ -26,7 +28,10 @@ if(isset($_SESSION['safe_key']) && isset($_SESSION['user_id']) && isset($_SESSIO
                     WHERE H.rate = R.id AND C.id = H.living_place AND P.id = H.profession AND update_time > :date_from";
             $filename = 'user_update_history-' . date('d.m.Y') . '.csv';
         } else if ($_GET['id'] == 4){
-            $sql = "SELECT u.name AS user_name,u.surname,home.name as home_team,away.name as away_team,g.date_time,team_score_1,team_score_2,c.name AS court_name FROM user u,human_power hp,court c,game g JOIN team AS home ON g.team_id_1 = home.id JOIN team AS away ON g.team_id_2 = away.id where hp.game_id = g.id and hp.user_id = u.id and g.court_id = c.id and g.date_time > :date_from  ORDER BY G.date_time";
+            $sql = "SELECT u.name AS user_name,u.surname,home.name as home_team,away.name as away_team,g.date_time,team_score_1,team_score_2,c.name AS court_name 
+                    FROM user u,human_power hp,court c,game g JOIN team AS home ON g.team_id_1 = home.id JOIN team AS away ON g.team_id_2 = away.id 
+                    WHERE hp.game_id = g.id and hp.user_id = u.id and g.court_id = c.id and g.date_time > :date_from  
+                    ORDER BY G.date_time";
             $filename = 'user_match_history-' . date('d.m.Y') . '.csv';
         }
         header('Content-type: text/csv');
