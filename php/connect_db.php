55490340c8e95919908c2189e1c64f16f36a8284
Silvan Sholla <silvansholla@gmail.com>
2020-06-09 11:09:23 +0300

    connect db fix

--- php/connect_db.php
+++ php/connect_db.php
@@ -11,12 +11,12 @@ $pass = '';
 // connect to database or return error
 try
 {
- $dbh = new PDO("mysql:unix_socket=$host;dbname=$dbname;charset=utf8", $user,$pass);
+ $dbh = new PDO("mysql:unix_socket=$host;dbname=$dbname;charset=utf8mb4", $user,$pass);
  $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
- $dbh->query('set character_set_client=utf8');
- $dbh->query('set character_set_connection=utf8');
- $dbh->query('set character_set_results=utf8');
- $dbh->query('set character_set_server=utf8');
+ $dbh->query('set character_set_client=utf8mb4');
+ $dbh->query('set character_set_connection=utf8mb4');
+ $dbh->query('set character_set_results=utf8mb4');
+ $dbh->query('set character_set_server=utf8mb4');
  }
 catch(PDOException $e)
 {
